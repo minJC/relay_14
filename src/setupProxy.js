00cef208f5a0cb60a5ae024d73d2069060e00a71
dogyeong <shroad1802@naver.com>
2020-08-15 15:15:41 +0900

    update: proxy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,7 +3,7 @@ module.exports = function (app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: 'http://localhost:5000',
+      target: 'https://relay14-server.herokuapp.com/',
       changeOrigin: true,
     })
   );
