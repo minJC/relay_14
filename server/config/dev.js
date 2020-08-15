da6578a81e5708ba1602e7aa129552d880d90164
tejava <goddrinktejava@gmail.com>
2020-08-16 00:59:07 +0900

    fixed HPM issue caused from uneditted merge conflict in node config/dev.js

--- server/config/dev.js
+++ server/config/dev.js
@@ -1,11 +1,4 @@
 module.exports = {
-<<<<<<< HEAD
   mongoURI:
     "mongodb+srv://boostcamp:1q2w3e4r@boostcamp.vnzwn.gcp.mongodb.net/test?retryWrites=true&w=majority",
 };
-=======
-    //mongoURI:'mongodb+srv://ycg02116:abcd1234@boilerplate-j0sts.mongodb.net/test?retryWrites=true&w=majority'
-    // mongoURI:'mongodb+srv://gyu1107:19690427@boilerplate-j0sts.mongodb.net/test?retryWrites=true&w=majority'
-    mongoURI: "mongodb+srv://boostcamp:1q2w3e4r@boostcamp.vnzwn.gcp.mongodb.net/test?retryWrites=true&w=majority"
-}
->>>>>>> 0c3b78d7c16a9f09c71e9775ee5bd27ce156eae3
